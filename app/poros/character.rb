ddc00175ff64826db780f2cc530ceccab0ccbfb1
Gaby Mendez <gmendez90@gmail.com>
2020-09-01 12:58:43 -0400

    Add css id to member-details, add :_id to Character poro, check for specific character name in spec

--- app/poros/character.rb
+++ app/poros/character.rb
@@ -1,9 +1,10 @@
 class Character
-  attr_reader :name, :house, :role, :patronus
+  attr_reader :id, :name, :house, :role, :patronus
   def initialize(character_hash)
+    @id = character_hash[:_id]
     @name = character_hash[:name]
     @house = character_hash[:house]
     @role = character_hash[:role] if character_hash[:role]
     @patronus = character_hash[:patronus] if character_hash[:patronus]
-  end 
+  end
 end
