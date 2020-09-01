71ce985bf1baf374c02cf0a3460bf0412c22300b
Gaby Mendez <gmendez90@gmail.com>
2020-09-01 12:31:54 -0400

    Write api call in SearchController

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,20 @@
 class SearchController < ApplicationController
   def index
     @house = params[:house]
+
+    conn = Faraday.new("https://www.potterapi.com/v1/") do |req|
+      req.params['key'] = ENV['POTTER_API_KEY']
+    end
+
+    response = conn.get("characters") do |req|
+      req.params['house'] = @house
+      req.params['orderOfThePhoenix'] = true
+    end
+
+    members_hash = JSON.parse(response.body, symbolize_names: true)
+
+    @members = members_hash.map do |hash|
+      Character.new(hash)
+    end
   end
 end
